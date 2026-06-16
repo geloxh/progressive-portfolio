5544a099a9197f6dfb43aea6a4c42e73f578cfa2
geloxh <ace.blazado7@gmail.com>
2026-06-16 20:39:00 +0800

    initial ProjectService file

--- backend-portfolio/Services/ProjectService.js
+++ backend-portfolio/Services/ProjectService.js
@@ -1,1 +1,36 @@
-ECHO is on.
+const Project = require('../models/Project')
+
+const projectService = {
+
+  getAll: async () => {
+    return await Project.find().sort({ createdAt: -1 })
+  },
+
+  getById: async (id) => {
+    const project = await Project.findById(id)
+    if (!project) {
+      const err = new Error('Project not found')
+      err.status = 404
+      throw err
+    }
+    return project
+  },
+
+  create: async (data) => {
+    const project = new Project(data)
+    return await project.save()
+  },
+
+  update: async (id, data) => {
+    return await Project.findByIdAndUpdate(id, data, {
+      new: true,           // return updated doc
+      runValidators: true, // run schema validators
+    })
+  },
+
+  delete: async (id) => {
+    return await Project.findByIdAndDelete(id)
+  },
+}
+
+module.exports = projectService
