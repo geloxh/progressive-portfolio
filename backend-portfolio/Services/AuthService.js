51f824fdd60feea5aefa9a1c06392cab37eca817
geloxh <ace.blazado7@gmail.com>
2026-06-19 22:05:34 +0800

    Auth Service

--- backend-portfolio/Services/AuthService.js
+++ backend-portfolio/Services/AuthService.js
@@ -3,5 +3,29 @@ const jwt = require('jsonwebtoken')
 const Admin = require('./Models/Admin')
 
 const AuthService = {
-    
+    login: async (email, password) => {
+        const admin = await Admin.findOne({ email })
+        if (!admin) {
+            const err = new Error('Invalid credentials.')
+            err.status = 401
+            throw err
+        }
+
+        const isMatch = await bcrypt.compare(password, admin.password)
+        if (!isMatch) {
+            const err = new Error('Invalid credentials')
+            err.status = 401
+            throw err
+        }
+
+        const token = jwt.sign(
+            { id: admin._id, email: admin.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
+        )
+
+        return token
+    },
 }
+
+module.exports = AuthService
